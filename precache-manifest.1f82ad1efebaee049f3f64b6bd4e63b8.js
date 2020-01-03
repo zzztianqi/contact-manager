50be1009c4b7b02107c01a2a7535018d013af39e
zzztianqi <zhaotianqi.rex@gmail.com>
2020-01-04 00:04:00 +0800

    Updates

---
+++ precache-manifest.1f82ad1efebaee049f3f64b6bd4e63b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f57ea0da7645b2ec30e9cb3d35bd891",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "ed821c200dbfa6c7bac0",
+    "url": "/contact-manager/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ed821c200dbfa6c7bac0",
+    "url": "/contact-manager/static/js/2.0979c9f1.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/contact-manager/static/js/2.0979c9f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1e666ca44a822675feb",
+    "url": "/contact-manager/static/js/main.0f1600ff.chunk.js"
+  },
+  {
+    "revision": "0c9263cb7c07557d7218",
+    "url": "/contact-manager/static/js/runtime-main.ebdc3130.js"
+  }
+]);
